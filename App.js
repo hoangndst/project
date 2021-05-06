a2f381265d802360480f3fa91502910c6a632051
hoangndst <hoangndst@outlook.com>
2021-05-06 20:12:49 +0700

    ok

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, StatusBar } from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log("Pressed");
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text onPress={handlePress}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Image style={imgStyle.icon} source={require('./assets/icon.png')}/>
+      <StatusBar style='dark-content'/>
+    </SafeAreaView>
   );
 }
+
+const imgStyle = StyleSheet.create({
+  icon: {
+    width: 50,
+    height: 50
+  }
+});
 
 const styles = StyleSheet.create({
   container: {
