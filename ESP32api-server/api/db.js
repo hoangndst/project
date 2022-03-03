eeeeaf14de5cd118fd5b00507da1e8ba4718e1d9
hoangndst <hoangndst@outlook.com>
2022-03-04 01:37:13 +0700

    new server

--- ESP32api-server/api/db.js
+++ ESP32api-server/api/db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql');
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
-  port: 3307,
-  user: process.env.DB_USER || "root",
-  password: "password",
+  port: 3306,
+  user: process.env.DB_USER || "hoangndst",
+  password: "Hoang2002*",
   database: process.env.DB_NAME || "esp32_api"
 });
 
