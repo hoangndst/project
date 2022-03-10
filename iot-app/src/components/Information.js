1c7260d8671d0f138f305fce960ae63f3af926de
hoangndst <hoangndst@outlook.com>
2022-03-10 23:57:46 +0700

    add dht11

--- iot-app/src/components/Information.js
+++ iot-app/src/components/Information.js
@@ -1,9 +1,9 @@
-import { Grid, Paper, Alert, Typography, FormControlLabel, Skeleton, Button } from '@mui/material'
-import Switch from '@mui/material/Switch';
+import { Grid, Paper, Alert, Typography } from '@mui/material'
+// import Switch from '@mui/material/Switch';
 import { styled } from '@mui/material/styles'
 import React from 'react'
 import { useEffect } from 'react';
-import { DatePicker } from '@mui/lab';
+// import { DatePicker } from '@mui/lab';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import CalendarPicker from '@mui/lab/CalendarPicker';
@@ -11,38 +11,38 @@ import { getDht11Data } from '../controller/action';
 import { Line } from 'react-chartjs-2';
 import Chart from 'chart.js/auto'
 
-const Android12Switch = styled(Switch)(({ theme }) => ({
-    padding: 8,
-    '& .MuiSwitch-track': {
-      borderRadius: 22 / 2,
-      '&:before, &:after': {
-        content: '""',
-        position: 'absolute',
-        top: '50%',
-        transform: 'translateY(-50%)',
-        width: 16,
-        height: 16,
-      },
-      '&:before': {
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
-          theme.palette.getContrastText(theme.palette.primary.main),
-        )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
-        left: 12,
-      },
-      '&:after': {
-        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
-          theme.palette.getContrastText(theme.palette.primary.main),
-        )}" d="M19,13H5V11H19V13Z" /></svg>')`,
-        right: 12,
-      },
-    },
-    '& .MuiSwitch-thumb': {
-      boxShadow: 'none',
-      width: 16,
-      height: 16,
-      margin: 2,
-    },
-}));
+// const Android12Switch = styled(Switch)(({ theme }) => ({
+//     padding: 8,
+//     '& .MuiSwitch-track': {
+//       borderRadius: 22 / 2,
+//       '&:before, &:after': {
+//         content: '""',
+//         position: 'absolute',
+//         top: '50%',
+//         transform: 'translateY(-50%)',
+//         width: 16,
+//         height: 16,
+//       },
+//       '&:before': {
+//         backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+//           theme.palette.getContrastText(theme.palette.primary.main),
+//         )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
+//         left: 12,
+//       },
+//       '&:after': {
+//         backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+//           theme.palette.getContrastText(theme.palette.primary.main),
+//         )}" d="M19,13H5V11H19V13Z" /></svg>')`,
+//         right: 12,
+//       },
+//     },
+//     '& .MuiSwitch-thumb': {
+//       boxShadow: 'none',
+//       width: 16,
+//       height: 16,
+//       margin: 2,
+//     },
+// }));
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
